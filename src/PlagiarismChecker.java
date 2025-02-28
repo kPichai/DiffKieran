41fd603fe2b2443989f0b330f1ffb13aaf1dc861
kpi <kieran.pichai@menloschool.org>
2025-02-27 16:40:28 -0800

    aosijd gj04wowei

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -14,19 +14,26 @@ public class PlagiarismChecker {
      * @param doc2 the second
      * @return The length of the longest shared substring.
      */
+
+    // Tabulation focused approach to the longest shared substring problem, takes in two strings
     public static int longestSharedSubstring(String doc1, String doc2) {
+        // Create a 2d array to store lengths of shared substrings
         int[][] subsequenceLength = new int[doc1.length()+1][doc2.length()+1];
 
+        // Loop through each character of both strings
         for (int i = 1; i <= doc1.length(); i++) {
             for (int j = 1; j <= doc2.length(); j++) {
                 if (doc1.charAt(i - 1) == doc2.charAt(j - 1)) {
+                    // If characters match increment the count from the previous diagonal cell
                     subsequenceLength[i][j] = subsequenceLength[i-1][j-1] + 1;
                 } else {
+                    // Otherwise take the maximum value of the left or top cell
                     subsequenceLength[i][j] = Math.max(subsequenceLength[i-1][j], subsequenceLength[i][j-1]);
                 }
             }
         }
 
+        // Return bottom-right cell contains as it contains the length of the longest shared substring
         return subsequenceLength[doc1.length()][doc2.length()];
     }
 }
